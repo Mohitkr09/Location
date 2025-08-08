d6a0d52364c38cefa9b4cf0c1fb03ec14bcb81aa
Mohitkr09 <mkt27858@gmail.com>
2025-08-08 12:41:05 +0530

    complete Navbar

---
+++ project1/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Navbar from './Navbar';
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <h1>Welcome to My Site</h1>
+    </div>
+  );
+}
+
+export default App;
+
---
+++ project1/src/Navbar.jsx
@@ -0,0 +1,30 @@
+import React, { useState } from 'react';
+import './Navbar.css';
+
+const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
+  return (
+    <nav className="navbar">
+      <div className="logo">MySite</div>
+
+      <ul className={`nav-links ${isOpen ? 'open' : ''}`}>
+        <li><a href="#">Home</a></li>
+        <li><a href="#">About</a></li>
+        <li><a href="#">Services</a></li>
+        <li><a href="#">Contact</a></li>
+      </ul>
+
+      <div className="hamburger" onClick={toggleMenu}>
+        ☰
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
+
